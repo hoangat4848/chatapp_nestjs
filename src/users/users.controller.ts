cbc64a716d4c87da479c32b809f9004215ed3909
hoangat <hoangat4848@gmail.com>
2022-12-23 22:44:41 +0700

    Implement profile endpoint and accept image upload

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,15 @@
 import { Controller, HttpException, HttpStatus, Inject } from '@nestjs/common';
-import { Get, Query } from '@nestjs/common/decorators';
+import {
+  Body,
+  Get,
+  Post,
+  Query,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common/decorators';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { Routes, Services } from 'src/utils/constants';
+import { UserProfileDto } from './dtos/UserProfile.dto';
 import { IUsersService } from './user';
 
 @Controller(Routes.USERS)
@@ -15,5 +24,15 @@ export class UsersController {
       throw new HttpException('Provide a valid query', HttpStatus.BAD_REQUEST);
 
     return this.usersService.searchUsers(query);
+  }
+
+  @Post('profiles')
+  @UseInterceptors(FileInterceptor('file'))
+  async completeProfile(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() userProfileDto: UserProfileDto,
+  ) {
+    console.log(file);
+    console.log(userProfileDto.about, userProfileDto.username);
   }
 }
